2b784f7036ac04ad33d6244f8883f39472d093f7
Администратор <@WIN-R3T3CJ5EAGK>
2023-01-11 10:23:43 +0300

    Initial commit2

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ win.title('Address Entry Form')
 
 from_form=tk.Frame(relief=tk.SUNKEN,borderwidth=3)
 from_form.pack()
+labels=[
+    'Имя',"фамилия:",'Адрес1','Адрес2','Город','Регион','Почтовый индекс','Страна',]
+# Цикл для списка ярлыков полей.
+for idx,text in enumerate(labels):
+    label=tk.Label(master=from_form,text=text)
+    entry=tk.Entry(master=from_form,width=50)
+    label.grid(row=idx,column=0,sticky='e')
+    entry.grid(row=idx,column=1)
 
 win.mainloop()
